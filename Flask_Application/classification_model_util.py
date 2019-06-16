a0709ddb12b4876639cb220f6def2c9b8c7098e8
Aman <jiddewar.amandeep@gmail.com>
2019-06-16 00:40:17 -0400

    aws s3

--- Flask_Application/classification_model_util.py
+++ Flask_Application/classification_model_util.py
@@ -8,6 +8,8 @@ from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectKBest
 from sklearn.pipeline import Pipeline
+import boto3
+import sys
 
 
 class ModelUtil:
@@ -22,7 +24,8 @@ class ModelUtil:
         return self.model
 
     def train_model(self):
-        df = pd.read_csv("shuffled-full-set-hashed.csv", header=None)
+        #df = pd.read_csv("shuffled-full-set-hashed.csv", header=None)
+        df = self.load_data()
         df.columns = ['Category', 'Content']
 
         (features_train, labels_train) = (df.loc[1:40, 'Content'].values.astype('U'), df.loc[1:40, 'Category'])
@@ -42,6 +45,28 @@ class ModelUtil:
         linearsvc.fit(features_train, labels_train)
         self.model = linearsvc
 
+    def load_data(self):
+        if sys.version_info[0] < 3:
+            from StringIO import StringIO  # Python 2.x
+        else:
+            from io import StringIO  # Python 3.x
+
+        # get your credentials from environment variables
+        aws_id = 'AKIAI6MTK372VSBZUJCQ'
+        aws_secret = 'SIVH9SQDAEtmz7WrPzcXa6cvVS2ox0lPMl3T6Z3o'
+
+        client = boto3.client('s3', aws_access_key_id=aws_id,
+                              aws_secret_access_key=aws_secret)
+
+        bucket_name = 'classification-data-hw'
+
+        object_key = 'shuffled-full-set-hashed.csv'
+        csv_obj = client.get_object(Bucket=bucket_name, Key=object_key)
+        body = csv_obj['Body']
+        csv_string = body.read().decode('utf-8')
+
+        df = pd.read_csv(StringIO(csv_string), header=None)
+        return df
 
 model_util = ModelUtil()
 classifier = model_util.get_model()
